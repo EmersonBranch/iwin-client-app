172e684f3f4d831e99c6a85c8982e4bfa28407b2
ederson-abreu <ederson@branchit.com.br>
2024-10-14 19:30:02 -0300

    correcao animacao

--- iwin-app-client/src/app/loading/loading.page.ts
+++ iwin-app-client/src/app/loading/loading.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadingPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['/onboarding']);
+    }, 4000); 
   }
 
 }
